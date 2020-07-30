5a3777856f33e3c89b9b51f273a4f3aa95b03bfa
Yash Indane <53041219+YashIndane@users.noreply.github.com>
2020-07-30 18:31:47 +0530

    Update countours_detect.py

--- countours_detect.py
+++ countours_detect.py
@@ -13,7 +13,41 @@ def check_if_square(P1 , P2) -> bool :
    return abs(d1 - d2)  < 2
 
 
+def sort_array(TA) -> list : 
+   TA = sorted(TA ,  key = lambda x : x[1])
+
+   Z1 = TA[0 : 3]
+   Z1.sort()
+   Z2 = TA[3 : 6]
+   Z2.sort()
+   Z3 = TA[6 : 9]
+   Z3.sort()
+   
+   
+
+   i = 0
+   print(Z1 + Z2 + Z3)
+
+   
+   for x , y in  Z1 + Z2 + Z3  : 
+      img = img_org[y + 10 : y + 35 , x + 10 : x + 35]
+      cv2.imwrite(str(i) + '.png' , img)
+      i += 1
+
+  
+
+
+      
+
+
+
+
+
+
+
 tile_coordinates = []
+
+
 while True : 
     success, img_org = cap.read()
 
@@ -22,7 +56,7 @@ while True :
     canny = cv2.Canny(blurred, 20, 40)
 
     kernel = np.ones((3,3), np.uint8)
-    dilated = cv2.dilate(canny, kernel, iterations = 1)
+    dilated = cv2.dilate(canny, kernel, iterations = 0)
 
     (contours, hierarchy) = cv2.findContours(dilated.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -30,20 +64,22 @@ while True :
 
 
 
-    tile_counter = 0
-    countours_counter = 0
+    
+    
     font = cv2.FONT_HERSHEY_COMPLEX 
+    
 
 
     for cnt in contours : 
 
          area = cv2.contourArea(cnt) 
-         coordinates = []
+        
          
     
             # Capturing grid squares by area 
-         if area < 3000 and area > 1000 :  
+         if area < 3500 and area > 1500 :  
                 approx = cv2.approxPolyDP(cnt,   0.03 * cv2.arcLength(cnt, True), True) 
+                coordinates = []
     
                 # Only grabbing 4 sided polygons
                 if(len(approx) == 4):  
@@ -69,16 +105,22 @@ while True :
                       if check_if_square(P1 , P2) : 
 
                         cv2.drawContours(img_org, [approx], 0, (20, 255 , 57), 2)
+                        
 
-                        if all([abs(P1[0][0] - x) > 50 or abs(P1[0][1] - y) > 50 for x , y in tile_coordinates]) : 
+                        if all([abs(P1[0][0] - x) > 20 or abs(P1[0][1] - y) > 20 for x , y in tile_coordinates]) :
+                              #cv2.drawContours(img_org, [approx], 0, (20, 255 , 57), 2) 
 
                               tile_coordinates.append(P1[0])
+                              
+
+                              
 
                               if len(tile_coordinates) == 9 : 
 
                                  print(tile_coordinates)
+                                 sort_array(tile_coordinates)
                                  tile_coordinates = []
-
+                                 
                                   
                               
                         
